39305d2c93c59e67f265f7ce3f46d95bf85f2988
Alejandro <ajgarciabouzada@gmail.com>
2023-04-09 12:03:10 -0300

    Agregar archivos de proyecto.

---
+++ SOB_I01/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sumador;
+
+namespace SOB_I01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio I01 - Sumador");
+
+            Sumador.Sumador s1 = new Sumador.Sumador();
+
+            Sumador.Sumador s2 = new Sumador.Sumador(5);
+
+            Console.WriteLine("Cantidad de sumas en s2: {0}", ((int)s1));
+            Console.WriteLine("Cantidad de sumas en s2: {0}", ((int)s2));
+
+            Console.WriteLine("Suma 1: {0}", s1.Sumar(450, 150));
+            Console.WriteLine("Suma 2: {0}", s1.Sumar(s1.Sumar(25,175), 150));
+            Console.WriteLine("Suma 4: {0}", s1.Sumar(s1.Sumar("150 + 300 ", "+ 550 "), "= 1000"));
+            Console.WriteLine("La cantidad de sumas de s1 y s2 son iguales: {0}", s1 | s2);
+            Console.WriteLine("Suam 3: {0}", s1.Sumar("2 + 2 = ", "4"));
+            Console.WriteLine("La sumas de s1 y s2 es: {0}", s1 + s2);
+            Console.WriteLine("Suma 5: {0}", s1.Sumar(1920, 1080));
+            Console.WriteLine("La cantidad de sumas de s1 y s2 son iguales: {0}", s1 | s2);
+
+
+        }
+    }
+}
---
+++ Sumador/Sumador.cs
@@ -0,0 +1,46 @@
+﻿namespace Sumador
+{
+    public class Sumador
+    {
+        public int CantidadSumas { get => _cantidadSumas; set => _cantidadSumas = value; }
+        private int _cantidadSumas;
+
+        public Sumador(int inicio)
+        {
+            CantidadSumas = inicio;
+        }
+
+        public Sumador()
+        {
+            CantidadSumas = 0;
+        }
+
+        public long Sumar(long a, long b)
+        {
+            CantidadSumas++;
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            CantidadSumas++;
+            return a + b;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.CantidadSumas + s2.CantidadSumas;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            return s1.CantidadSumas == s2.CantidadSumas;
+        }
+
+        public static explicit operator int(Sumador s)
+        {
+            return s.CantidadSumas;
+        }
+        
+    }
+}
